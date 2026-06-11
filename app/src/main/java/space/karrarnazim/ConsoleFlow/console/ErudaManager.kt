d803a063eec1afc3c26611971ae603a84362dc8c
SANDRO00O <mk63cp@gmail.com>
2026-06-11 08:29:57 +0300

    update

--- app/src/main/java/space/karrarnazim/ConsoleFlow/console/ErudaManager.kt
+++
@@ -1,7 +0,0 @@
-package space.karrarnazim.ConsoleFlow
-
-object ErudaManager {
-    fun initScript(): String = ConsoleScripts.initScript()
-    fun disableScript(): String = ConsoleScripts.disableScript()
-    fun touchHookScript(): String = ConsoleScripts.touchHookScript()
-}
