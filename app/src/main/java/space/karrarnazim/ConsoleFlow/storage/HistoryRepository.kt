9459686552e159b0b0b726f2a4d2fdea47ed2e6c
SANDRO00O <mk63cp@gmail.com>
2026-06-12 02:30:26 +0300

    update

--- app/src/main/java/space/karrarnazim/ConsoleFlow/storage/HistoryRepository.kt
+++
@@ -1,9 +0,0 @@
-package space.karrarnazim.ConsoleFlow
-
-class HistoryRepository(private val prefsManager: PrefsManager) {
-    fun addHistory(title: String, url: String) = prefsManager.addHistory(title, url)
-
-    fun clearHistory() = prefsManager.clearHistory()
-
-    fun getHistory(): List<Pair<String, String>> = prefsManager.getHistory()
-}
